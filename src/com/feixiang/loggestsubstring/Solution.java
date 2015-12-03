7ee837c0a6158c0b6cef2338c2e820d516406c6f
Administrator <Administrator@WIN-GURD9NCG0MD>
2015-12-03 23:35:11 +0800

    loggestsubstring

--- src/com/feixiang/loggestsubstring/Solution.java
+++ src/com/feixiang/loggestsubstring/Solution.java
@@ -1,35 +1,62 @@
 package com.feixiang.loggestsubstring;
 
+import java.util.HashSet;
+
+/**
+ * set 去重功能
+ * @author Administrator
+ *
+ */
 public class Solution {
 	public static int lengthOfLongestSubstring(String s) {
-		char[] ch = s.toCharArray();
-		int length = 1;
-		for (int i = 0; i < ch.length; i++) {
-			if(ch.length-i <= length){
-				 break;
-			 }
-			int size = 1;
-			for (int j = i + 1; j < ch.length; j++) {
-				int flag = 0;
-				for(int k = i; k < j;k++){
-					if(ch[j] == ch[k]){
-						flag = 1;
-						i = k;
-					}
+//		char[] ch = s.toCharArray();
+//		int length = 1;
+//		for (int i = 0; i < ch.length; i++) {
+//			if(ch.length-i <= length){
+//				 break;
+//			 }
+//			int size = 1;
+//			for (int j = i + 1; j < ch.length; j++) {
+//				int flag = 0;
+//				for(int k = i; k < j;k++){
+//					if(ch[j] == ch[k]){
+//						flag = 1;
+//						i = k;
+//					}
+//				}
+//				if (flag == 0) {
+//					size++;
+//					length = size > length ? size : length;
+//				} else {
+//					break;
+//				}
+//			}
+//			 
+//		}
+//		return length;
+		int strLength = s.length();
+		int maxLength = 0;
+		int walker = 0;
+		int runner = 0;
+		HashSet<Character> set = new HashSet<Character>();
+		for(int i = 0 ;i < strLength ; i++){
+			if(!set.contains(s.charAt(i))){
+				set.add(s.charAt(i));
+			}else{
+				maxLength = maxLength < runner - walker ? runner - walker : maxLength;
+				while(s.charAt(walker) != s.charAt(runner)){
+					set.remove(s.charAt(walker));
+					walker ++;
 				}
-				if (flag == 0) {
-					size++;
-					length = size > length ? size : length;
-				} else {
-					break;
-				}
+				walker ++;
 			}
-			 
+			runner ++;
 		}
-		return length;
+		maxLength = maxLength > runner - walker ? maxLength:runner - walker;
+		return maxLength;
 	}
 
 	public static void main(String[] args) {
-		System.out.println(lengthOfLongestSubstring("abcabc"));
+		System.out.println(lengthOfLongestSubstring("tmmzuxt"));
 	}
 }
